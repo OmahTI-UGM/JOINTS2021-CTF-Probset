944d8cebe4ffada540fcc06cdb460399fd4921af
Rafie Muhammad <rafie.muhammad@gdplabs.id>
2021-03-29 10:37:06 +0800

    add last chall to crypto final 'ReSAh', finalize

--- Final/Crypto/here_we_aes_again/peserta/chall.py
+++ Final/Crypto/here_we_aes_again/peserta/chall.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 from Crypto.Cipher import AES
+from Crypto.Util import Counter
 import os
-import sys
-
+import struct
+import json
+import zlib
 import sys
 
 class Unbuffered(object):
@@ -31,6 +33,23 @@ def decrypt(enc, key, nonce, tag):
     except:
         return None
 
+def crc(inp):
+    return 0xffffffff ^ zlib.crc32(inp)
+
+def gen_extra_code(json_str, key, aes_key):
+    code = json_str.encode() + key
+    code = code + struct.pack('<L', crc(code))
+    aes_obj = AES.new(aes_key, AES.MODE_CTR, counter = Counter.new(128))
+    return aes_obj.encrypt(code)
+
+def verify_extra_code(code, key, aes_key):
+    aes_obj = AES.new(aes_key, AES.MODE_CTR, counter = Counter.new(128))
+    json_str = aes_obj.decrypt(code)
+    if json_str[-20:-4] == key:
+        return json_str[:-20]
+    else:
+        return None
+
 
 print("""===========================
 Super Secret Agent Service
@@ -39,11 +58,15 @@ Super Secret Agent Service
 
 flag = open("flag.txt").read()
 key = os.urandom(16)
+another_key = os.urandom(16)
+another_another_key = os.urandom(16)
 nonce = os.urandom(16)
 admin_code = os.urandom(16)
+regular_extra_code = gen_extra_code(json.dumps({"adm00n": 0}), another_another_key, another_key)
 
 print(f"Here is the admin code : {admin_code.hex()}")
-print("But here, we implement tag as 2fa for admin\n")
+print(f"Here is the extra code for regular user : {regular_extra_code.hex()}")
+print("Here, we implement double protection for admin\n")
 print("""Menu :
 1. Generate Encrypted Code
 2. Enter as agent""")
@@ -72,8 +95,17 @@ while True:
             verify = decrypt(enc_code, key, nonce, code_tag)
             
             if verify == admin_code:
-                print(f"Welcome admin, here is your flag : {flag}")
-                exit()
+                print("We identify you as admin, but let us check once again")
+                extra_code = input("Enter Extra Code : ")
+                extra_code = bytes.fromhex(extra_code)
+                admin_json = json.loads(verify_extra_code(extra_code, another_another_key, another_key))
+
+                if admin_json["adm00n"]:
+                    print(f"Welcome admin, here is your flag : {flag}")
+                    exit()
+                else:
+                    print("Unfortunate, your extra code is wrong")
+
             else:
                 print("Welcome agent, have fun here :)")
 
--- Final/Crypto/here_we_aes_again/solver/solve.py
+++ Final/Crypto/here_we_aes_again/solver/solve.py
@@ -1,10 +1,28 @@
 from pwn import *
+import json
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+import struct
+import os
+import zlib
+
+def crc(inp):
+    return 0xffffffff ^ zlib.crc32(inp)
+
+def gen_extra_code(json_str, key, aes_key):
+    code = json_str.encode() + key
+    code = code + struct.pack('<L', crc(code))
+    aes_obj = AES.new(aes_key, AES.MODE_CTR, counter = Counter.new(128))
+    return aes_obj.encrypt(code)
 
 while True:
     r = remote("localhost", 5000)
 
     r.recvuntil("admin code : ")
     admin_code = r.recvline()[:-1].decode()
+
+    r.recvuntil("regular user : ")
+    extra_code = r.recvline()[:-1].decode()
     
     r.sendlineafter("> ", "1")
 
@@ -33,8 +51,19 @@ while True:
 
     result = r.recv()
 
-    if b"JOINTS21{" in result:
-        print(result)
+    if b"identify" in result:
+        mac_key = os.urandom(0x10)
+        key_1 = os.urandom(16)
+        key_2 = os.urandom(16)
+
+        cipher_1 = gen_extra_code(json.dumps({'adm00n': 0}), key_1, key_2)
+        cipher_2 = gen_extra_code(json.dumps({'adm00n': 1}), key_1, key_2)
+        xor_cipher = xor(cipher_1, cipher_2)
+        final = xor(xor_cipher, bytes.fromhex(extra_code))
+        
+        r.sendline(final.hex())
+
+        print(r.recv())
         exit()
 
     r.close()
