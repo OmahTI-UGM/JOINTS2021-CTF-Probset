71fc61520274ce9bd5d33e4210ad94c6a009d954
Fadli Maulana <wetmanz23@gmail.com>
2021-04-25 15:05:55 +0700

    Update script.py

--- Final/Web/joints-pay/server/script.py
+++ Final/Web/joints-pay/server/script.py
@@ -5,11 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# account = {
-#     "email": "bambangbajigur1@gmail.com",
-#     "password": "passwordsementara"
-# }
-
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
@@ -22,46 +17,6 @@ def wait_until(ec, by, value):
 
 url = "http://jointspay-nginx/"
 driver.get(url + 's3cr3t_c0d3.php?p=8682a65aa7f080e0d8511018a9f64d77')
-
-# loginDiscordXPath = "/html[1]/body[1]/div[1]/div[1]/a[1]/p[1]"
-# loginDiscordButton = wait_until(
-#     EC.element_to_be_clickable,
-#     By.XPATH,
-#     loginDiscordXPath
-# )
-# loginDiscordButton.click()
-
-# discordEmailFormXPath = "/html[1]/body[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[2]/input[1]"
-# discordPasswordFormXPath = "/html[1]/body[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/div[3]/div[2]/div[1]/input[1]"
-# discordLoginButtonXPath = "/html[1]/body[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/div[3]/button[2]/div[1]"
-
-# discordEmailForm = wait_until(
-#     EC.presence_of_element_located,
-#     By.XPATH,
-#     discordEmailFormXPath
-# )
-# discordPasswordForm = wait_until(
-#     EC.presence_of_element_located,
-#     By.XPATH,
-#     discordPasswordFormXPath
-# )
-# discordLoginButton = wait_until(
-#     EC.element_to_be_clickable,
-#     By.XPATH,
-#     discordLoginButtonXPath
-# )
-
-# discordEmailForm.send_keys(account['email'])
-# discordPasswordForm.send_keys(account['password'])
-# discordLoginButton.click()
-
-# discordAuthorizeButtonXPath = "/html[1]/body[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/button[2]"
-# discordAuthorizeButton = wait_until(
-#     EC.element_to_be_clickable,
-#     By.XPATH,
-#     discordAuthorizeButtonXPath
-# )
-# discordAuthorizeButton.click()
 
 while True:
     time.sleep(10)
